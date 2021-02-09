231f46de763579001af4359441b5cf667f65ef61
ADkun <adgnn@foxmail.com>
2021-02-09 21:55:40 +0800

    更新了解数独

--- src/solutions/array/IsValidSudoku.java
+++ src/solutions/array/IsValidSudoku.java
@@ -6,16 +6,24 @@ import java.util.Set;
 public class IsValidSudoku {
 
     public static void main(String[] args) {
-        char[][] board = {{'5','3','.','.','7','.','.','.','.'},
-                {'6','.','.','1','9','5','.','.','.'},
-                {'.','9','8','.','.','.','.','6','.'},
-                {'8','.','.','.','6','.','.','.','3'},
-                {'4','.','.','8','.','3','.','.','1'},
-                {'7','.','.','.','2','.','.','.','6'},
-                {'.','6','.','.','.','.','2','8','.'},
-                {'.','.','.','4','1','9','.','.','5'},
-                {'.','.','.','.','8','.','.','7','9'}};
-        new IsValidSudoku().solveSudoku(board);
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}};
+        IsValidSudoku obj =  new IsValidSudoku();
+        obj.solveSudoku(board);
+        for (char[] chars : board) {
+            for (char aChar : chars) {
+                System.out.print(aChar + "\t");
+            }
+            System.out.println();
+        }
     }
 
     private Set<Character>[] rows = new HashSet[9];
@@ -41,32 +49,38 @@ public class IsValidSudoku {
             }
         }
 
-        solve(board);
+        solve(0, 0, board);
     }
 
-    private boolean solve(char[][] board) {
-        for (int i = 0; i < board.length; ++i) {
-            for (int j = 0; j < board[0].length; ++j) {
-                if (board[i][j] == '.') {
-                    // 如果该位置是空
-                    for (char k = '1'; k <= '9'; ++k) {
-                        if (isValid(i, j, k, board)) {
-                            board[i][j] = k;
-                            if (solve(board))
-                                return true;
-                            else {// 回滚
-                                board[i][j] = '.';
-                                rows[i].remove(k);
-                                cols[j].remove(k);
-                                boxes[i / 3 * 3 + j / 3].remove(k);
-                            }
-                        }
+    private boolean solve(int i, int j, char[][] board) {
+        if (j == 9) {
+            j = 0;
+            ++i;
+        }
+        if (i == 9)
+            return true;
+
+        if (board[i][j] == '.') {
+            // 如果该位置是空
+            for (char k = '1'; k <= '9'; ++k) {
+                if (isValid(i, j, k, board)) {
+                    board[i][j] = k;
+                    if (solve(i, j + 1, board))
+                        return true;
+                    else {// 回滚
+                        board[i][j] = '.';
+                        rows[i].remove(k);
+                        cols[j].remove(k);
+                        boxes[i / 3 * 3 + j / 3].remove(k);
                     }
-                    return false;
                 }
             }
+            return false;
+        } else if (solve(i, j + 1, board)) {
+            return true;
+        } else {
+            return false;
         }
-        return true;
     }
 
     private boolean isValid(int i, int j, char k, char[][] board) {
